35e4d8daaa778ea8972210d0300ca3d16bda1a57
Iulian Cirlig <cirligiulian12312@gmail.com>
2024-01-18 02:10:27 +0200

    JS Update: finished taber implementation

--- js/app.js
+++ js/app.js
@@ -1,27 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", () => {
 
+  const mediaQuery = "(min-width: 992px)";
+  const desktop = window.matchMedia(mediaQuery);
 
-  console.log("loaded");
   // Get all tab headings and tab content slides
   const tabHeadings = document.querySelectorAll(".tabs-headings button");
   const tabContentSlides = document.querySelectorAll(".tabs-content .slide");
 
   // Add click event listeners to each tab heading
-  tabHeadings.forEach(function (heading, index) {
-    heading.addEventListener("click", function () {
-      // Remove 'active' class from all tab headings and content slides
-      tabHeadings.forEach(function (h) {
+  tabHeadings.forEach((heading, index) => {
+    heading.addEventListener("click", () => {
+
+      tabHeadings.forEach((h) => {
         h.classList.remove("active");
       });
 
-      tabContentSlides.forEach(function (slide) {
+      heading.classList.add("active");
+
+      tabContentSlides.forEach((slide) => {
         slide.classList.remove("active");
+        setTimeout(() => {
+          slide.style.display = "none";
+        }, 50);
       });
 
-      // Add 'active' class to the selected tab heading and content slide
-      heading.classList.add("active");
-      tabContentSlides[index].classList.add("active");
+
+      const selectedSlide = tabContentSlides[index];
+
+      setTimeout(() => {
+        selectedSlide.style.display = index > 0 && desktop.matches ? "flex" : "block";
+      }, 100)
+
+      setTimeout(() => {
+        selectedSlide.classList.add("active");
+      }, 200);
+    });
+  });
+
+  // Add an event listener to handle the end of the transition
+  tabContentSlides.forEach((slide) => {
+    slide.addEventListener("transitionend", () => {
+      if (!slide.classList.contains("active")) {
+        slide.style.display = "none";
+      }
     });
   });
 });
